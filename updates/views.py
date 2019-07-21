c7566b8bb9388720d96c263ba36735eaf2ae0235
JAY <johnjohnsonokah@gmail.com>
2019-07-21 08:38:15 +0100

    Create a JSON response

--- updates/views.py
+++ updates/views.py
@@ -1,3 +1,16 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Update
+# def detail_view(request):
+# return render()  # return JSON --> JS Object Notation
+# return HttpResponse(get_template().render({}))
+
+
+def update_model_detail_view(request):
+    data = {
+        "count": 1000,
+        "content": "Some new content"
+    }
+    return JsonResponse(data)
